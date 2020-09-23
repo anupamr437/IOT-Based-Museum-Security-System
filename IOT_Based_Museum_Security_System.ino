028c52a18d9bcef9cdce5d73a97240fc9815a261
Anupam Roy <anupamr437@gmail.com>
2020-09-23 21:29:14 +0530

    Update IOT_Based_Museum_Security_System.ino

--- IOT_Based_Museum_Security_System.ino
+++ IOT_Based_Museum_Security_System.ino
@@ -1,14 +1,14 @@
 #define BLYNK_PRINT Serial
-#include <ESP8266WiFi.h> // It provides ESP8266 specific Wi-Fi routines that we are calling to connect to the network.
-#include <BlynkSimpleEsp8266.h> // It handles all the connection routines and data exchange between hardware, Blynk Cloud, and app.
+#include <ESP8266WiFi.h> // It provides ESP8266 microcontroller specific Wi-Fi routines that we are calling to connect to a network.
+#include <BlynkSimpleEsp8266.h> // It handles all the connection routines and data exchange between hardware, Blynk Cloud and Blynk app.
 #include<Servo.h> //It provides commands to control servo motors.
 char auth[] = "1c777027bb32498c92ffbaca882f5688"; // Device Authentication Token to get the hardware online and connect it to Blynk Cloud
-const int pirsensor=04; //D2
-const int ldr=00; // connect to pin D3 of NodeMCU development board, which connects to GPIO 00 of ESP8266 microcontroller.
-const int buzzerPin=02; //D4
-const int servo1=14; //D5
-const int servo2=12; //D6
-const int servo3=13; //D7
+const int pirsensor=04; // connect to pin D2 of NodeMCU development board, which connects to GPIO 04 of ESP8266 microcontroller
+const int ldr=00; // connect to pin D3 of NodeMCU development board, which connects to GPIO 00 of ESP8266 microcontroller
+const int buzzerPin=02; //connect to pin D4 of NodeMCU development board, which connects to GPIO 02 of ESP8266 microcontroller
+const int servo1=14; //connect to pin D5 of NodeMCU development board, which connects to GPIO 14 of ESP8266 microcontroller
+const int servo2=12; //connect to pin D6 of NodeMCU development board, which connects to GPIO 12 of ESP8266 microcontroller
+const int servo3=13; //connect to pin D7 of NodeMCU development board, which connects to GPIO 13 of ESP8266 microcontroller
 int pirValue;
 int ldrValue;
 Servo servoA;
